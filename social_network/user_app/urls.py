1daefc545e055ffb09e10a274a83a8f1a627c444
Dima <you@example.com>
2025-01-11 12:17:38 +0100

    Made comments. Made check for authentication and logout.

--- social_network/user_app/urls.py
+++ social_network/user_app/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('registration/', render_registration, name = "reg" ),
     path('login/', render_login, name = "login"),
-    path('welcome/', render_welcome, name = 'welcome')
+    path('welcome/', render_welcome, name = 'welcome'),
+    path('logout/', logout_user, name = 'logout')
 ]
--- social_network/user_app/views.py
+++ social_network/user_app/views.py
@@ -6,18 +6,26 @@ from django.contrib.auth import login, authenticate, logout
 
 
 def render_registration(request):
+    # Якщо метод запиту post (Якщо користувач відправив форму) 
     if request.method == 'POST':
+        # Зберігаємо дані з форми у змінні
         username = request.POST.get("username")
         password = request.POST.get("password")
         confirm_password = request.POST.get("confirm_password")
+        # Якщо користувач підтвердив пароль
         if password == confirm_password:
+            # відловлюємо помилку при спробі зареєструватися під вже існуючим ім'ям
             try:
+                # Заносимо користувача до бази даних (створюємо користувача)
                 User.objects.create_user(username = username, password = password)
+                # переадресовуємо користувача на сторінку логіна
                 return redirect('login')
             except IntegrityError:
+                # Відображаємо помилку на сторінці
                 messages.error(request=request, message="Такий користувач вже існує")
                 return redirect('reg')
         else:
+            # Відображаємо помилку на сторінці
             messages.error(request=request, message = 'Паролі не співпадають')
             return redirect('reg')
     return render(request = request, template_name = "user_app/registration.html")
@@ -27,8 +35,11 @@ def render_login(request):
     if request.method =="POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+        # Перевіряємо існування користувача по заданним данним | Дані коректні - повертає об'єкт User | Дані не коректні - повертає None
         user = authenticate(request=request, username=username, password=password)
+        # Якщо є такий користувач (Якщо користувач ввів вірні дані)
         if user != None:
+            # Авторизація користувача
             login(request, user)
             return redirect('welcome')
         else:
@@ -38,6 +49,14 @@ def render_login(request):
 
 
 def render_welcome(request):
-    return render(request, 'user_app/welcome.html')
-
+    # Якщо користувач авторизований
+    if request.user.is_authenticated:
+        return render(request, 'user_app/welcome.html')
+    else:
+       return redirect('login')
     
+def logout_user(request):
+    # Вихід користувача з акаунту
+    logout(request=request)
+    return redirect('login')
+
